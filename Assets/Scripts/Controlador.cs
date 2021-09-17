5bbe1a9153f7e5d87df8b8b2a54e060cb3af3e0d
Rochifuz <70174715+Rochifuz@users.noreply.github.com>
2021-09-17 10:39:41 -0300

    Nuevas escenas
    Escenas agregadas

---
+++ Assets/Scripts/Controlador.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controlador : MonoBehaviour
+{
+    public void cargarEscena(string name)
+    {
+        SceneManager.LoadScene(name);
+
+    }
+    public void salir()
+    {
+        Application.Quit();
+        Debug.Log(" Salir del juego");
+    }
+}
