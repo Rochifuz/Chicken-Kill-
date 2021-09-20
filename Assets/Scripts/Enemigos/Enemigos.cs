3a037a2429da7fb9b8640207f280d9fc2d0619f9
Rochifuz <70174715+Rochifuz@users.noreply.github.com>
2021-09-20 15:27:35 -0300

    Enemigos
    Seguimiento y creacion de enemigo

---
+++ Assets/Scripts/Enemigos/Enemigos.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigos : MonoBehaviour
+{
+    public float velocidad = 3;
+    private Rigidbody rbEnemigo;
+    private GameObject jugador;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbEnemigo = GetComponent<Rigidbody>();
+        jugador = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vectorAlObjetivo = (jugador.transform.position - transform.position).normalized;
+
+        rbEnemigo.AddForce(vectorAlObjetivo * velocidad);
+    }
+}
