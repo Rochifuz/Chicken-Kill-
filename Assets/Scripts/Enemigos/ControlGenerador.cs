c498fde1c5669add52d84c7dca1dbf959edb6712
Rochifuz <70174715+Rochifuz@users.noreply.github.com>
2021-09-20 15:57:10 -0300

    Oleada ciclo for
    Instanciar oleada de 3 enemigos

---
+++ Assets/Scripts/Enemigos/ControlGenerador.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlGenerador : MonoBehaviour
+{
+    public GameObject prefabEnemigos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GeneradorEnemigos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void GeneradorEnemigos ()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(prefabEnemigos, DamePosicionGeneracion(), prefabEnemigos.transform.rotation);
+        }
+    }
+
+    Vector3 DamePosicionGeneracion ()
+    {
+        float posXGeneracion = Random.Range(120, 150);
+        float posZGeneracion = Random.Range(750, 870);
+
+        Vector3 posAleatoria = new Vector3(posXGeneracion, 2, posZGeneracion);
+    
+        return posAleatoria;
+    }
+}
