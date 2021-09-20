33c32a005ad93f311d73f783749ac27a7e67751b
estefano99 <estefanobugari@gmail.com>
2021-09-20 14:50:35 -0300

    Update VolumenCodigo.cs
    Arreglo al error del volumen

--- Assets/Scripts/Volumen Cod/VolumenCodigo.cs
+++ Assets/Scripts/Volumen Cod/VolumenCodigo.cs
@@ -7,6 +7,7 @@ public class VolumenCodigo : MonoBehaviour
     public Slider slider;
     public float sliderValue;
     public Image imagenMute;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,7 @@ public class VolumenCodigo : MonoBehaviour
         RevisarSiEstoyMute();
 
     }
+
 
     public void ChangeSlider(float valor)
     {
@@ -33,4 +35,12 @@ public class VolumenCodigo : MonoBehaviour
             imagenMute.enabled = false;
         }
     }
+    void OnGUI()
+    {
+        //Create a horizontal Slider that controls volume levels. Its highest value is 1 and lowest is 0
+        sliderValue = GUI.HorizontalSlider(new Rect(25, 25, 200, 60), sliderValue, 0.0F, 1.0F);
+        //Makes the volume of the Audio match the Slider value
+        AudioListener.volume = sliderValue;
+    }
 }
+    
