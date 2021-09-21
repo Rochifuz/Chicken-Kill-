7b1a4ac4fcc89d377d5bceaaed41519a05c5c72a
FrancoLelli <franlelli1966@gmail.com>
2021-09-21 11:41:07 -0300

    Vida

---
+++ Assets/Scripts/Player/VidaPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VidaPlayer : MonoBehaviour
+{
+    public float vida = 100;
+
+    public Image barradeVida;
+
+
+
+ 
+    void Update()
+    {
+       vida = Mathf.Clamp(vida, 0, 100);
+       barradeVida.fillAmount = vida / 100;
+    }
+}
