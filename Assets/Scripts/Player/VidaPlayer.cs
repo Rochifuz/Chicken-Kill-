7545aeace0d34723912b5706498f0896a450094f
Rochifuz <70174715+Rochifuz@users.noreply.github.com>
2021-10-19 09:45:39 -0300

    Gallina que dispara

--- Assets/Scripts/Player/VidaPlayer.cs
+++ Assets/Scripts/Player/VidaPlayer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class VidaPlayer : MonoBehaviour
+public class VidaPlayer : MonoBehaviour 
 {
     public float vida = 100;
     
