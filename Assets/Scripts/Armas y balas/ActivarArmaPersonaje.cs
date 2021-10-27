1cd6f45a297834368e742dd00a04f6b8ede3c951
Rochifuz <70174715+Rochifuz@users.noreply.github.com>
2021-10-27 16:02:08 -0300

    Limpieza

--- Assets/Scripts/Armas y balas/ActivarArmaPersonaje.cs
+++ Assets/Scripts/Armas y balas/ActivarArmaPersonaje.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Este script se encuentra en el GameObject del Player
 public class ActivarArmaPersonaje : MonoBehaviour
 {
     public AgarrarArmas agarrarArma;
@@ -9,15 +9,15 @@ public class ActivarArmaPersonaje : MonoBehaviour
     // Start is called before the first frame update
     
   
-
+        //esta funcion se encarga de que cuando colisiona el player con el gameobject del arma se le active y destruya la que ya tiene
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-            agarrarArma = other.GetComponent<AgarrarArmas>();
-            agarrarArma.ActivarArmar(numeroArma);
-            agarrarArma.numeroArmaActiva = numeroArma;
-            Destroy(gameObject);
+            agarrarArma = other.GetComponent<AgarrarArmas>();//Esta obtiene el numero del arma con la que colisiona
+            agarrarArma.ActivarArmar(numeroArma); //Esta activa el arma segun su numero
+            agarrarArma.numeroArmaActiva = numeroArma; //esta me dice que arma activa tengo en la mano
+            Destroy(gameObject); //Esto destruye el gameobject del piso
         }
     }
 }
--- Assets/Scripts/Player/VidaPlayer.cs
+++ Assets/Scripts/Player/VidaPlayer.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+//este script se encuentra en el player
 public class VidaPlayer : MonoBehaviour
 {
     public float vida = 100;
     
     public Image barradeVida;
     
-
+    //funcion de muerte
     void Die()
     {
        Destroy(gameObject);        
     }
-
+    
     void Update()
     {
        vida = Mathf.Clamp(vida, 0, 100);
