35a76de5188b8c7d00ce54cb3b8a1c27fc2b8ad5
FrancoLelli <franlelli1966@gmail.com>
2021-10-27 16:33:35 -0300

    Comentarios vida player

--- Assets/Scripts/Player/VidaPlayer.cs
+++ Assets/Scripts/Player/VidaPlayer.cs
@@ -17,10 +17,11 @@ public class VidaPlayer : MonoBehaviour
     
     void Update()
     {
-       vida = Mathf.Clamp(vida, 0, 100);
-       barradeVida.fillAmount = vida / 100;
+       vida = Mathf.Clamp(vida, 0, 100);//Esta funcion llena un valor entre un minimo y maximo
+       barradeVida.fillAmount = vida / 100;//Ingresa a la propiedad de barra de vida llamada fill amount y calcula la vida actual sobre la vida maxima
         if (vida == 0)
         {
+            //Aca te va a mostrar si es que tu vida llega a 0, un cartel que perdiste y destruye el Game Object
             Ganar.show1();
             Die();
             Cursor.lockState = CursorLockMode.None;
