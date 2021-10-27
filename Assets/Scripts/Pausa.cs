a0cebded611623520b2bf6bfced108ba6d5a2c62
estefano99 <estefanobugari@gmail.com>
2021-10-27 16:35:19 -0300

    script Pausa comentada

--- Assets/Scripts/Pausa.cs
+++ Assets/Scripts/Pausa.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pausa : MonoBehaviour
+
+    // este script esta puesto en un empty object
 {
     bool pausa;
     // Start is called before the first frame update
@@ -14,7 +16,8 @@ public class Pausa : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Pause"))
+        // creamos en porject settings dentro de input Manager un input llamado " Pause", de valor P.
+        if (Input.GetButtonDown("Pause"))   
         {
             
             PausarJuego();
@@ -22,16 +25,16 @@ public class Pausa : MonoBehaviour
         }
     }
     
-    public void PausarJuego()
-    {
+    public void PausarJuego() // creamos una funcion que si el juego esta en funcionamiento y presionamos P, se pausay  viceversa.
+    {                         
         if (Time.timeScale == 1)
-        {
+        {                           // si presionamos P se pausa el juego , se activa la funcion del script Ganar que muestra los botones y se bloquea el cursos.
             Time.timeScale = 0;
-            Ganar.show2();
+            Ganar.show2();         
             Cursor.lockState = CursorLockMode.None;
         }
         else
-        {
+        {                       // Esto se ejecuta cuando presionamos de nuevo P , vuelvo a jugarse, se desactivan los botones de pausa y el cursor se bloquea
             Time.timeScale = 1;
             Ganar.botonPerderstatic.gameObject.SetActive(false);
             Ganar.botonJugarstatic.gameObject.SetActive(false);
