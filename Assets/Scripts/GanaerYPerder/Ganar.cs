07456f7dbae9bdb599001cd495918eb6162ae8b6
FrancoLelli <franlelli1966@gmail.com>
2021-10-27 16:41:50 -0300

    Comentarios en Ganar

--- Assets/Scripts/GanaerYPerder/Ganar.cs
+++ Assets/Scripts/GanaerYPerder/Ganar.cs
@@ -1,22 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Este script se encuentra en ManejadorGanarJuego
 public class Ganar : MonoBehaviour
 {
-    public GameObject Cartelganar;
+    public GameObject Cartelganar;//Cartel de cuando ganas
     public static GameObject ganarStatic;
-    public GameObject CartelPerder;
+    public GameObject CartelPerder;//Cartel de cuando perdes
     public static GameObject perderStatic;
 
     public static GameObject botonJugarstatic;
     public static GameObject botonPerderstatic;
-    public GameObject botonJugar;
-    public GameObject botonPerder;
+    public GameObject botonJugar;//Cartel de cuando volver a jugar
+    public GameObject botonPerder;//Cartel de cuando queres salir del juego
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Arrancan todos en falso
             Ganar.ganarStatic = Cartelganar;
         Ganar.ganarStatic.gameObject.SetActive(false);
 
@@ -36,6 +37,7 @@ public class Ganar : MonoBehaviour
         
     }
     public static void show()
+    //Esta funcion hace que te muestre un cartel cuando ganas
     {
         Ganar.ganarStatic.gameObject.SetActive(true);
         Ganar.botonJugarstatic.gameObject.SetActive(true);
@@ -43,6 +45,7 @@ public class Ganar : MonoBehaviour
     }
 
     public static void show1()
+    //Esta funcion hace que te muestre un cartel cuando perdes
     {
         Ganar.perderStatic.gameObject.SetActive(true);
         Ganar.botonPerderstatic.gameObject.SetActive(true);
@@ -50,6 +53,7 @@ public class Ganar : MonoBehaviour
     }
     
     public static void show2()
+    //Esta funcion hace que te muestre un cartel cuando pones pausa
     {
         Ganar.botonJugarstatic.gameObject.SetActive(true);
         Ganar.botonPerderstatic.gameObject.SetActive(true);
