b5bd244e8245fd4d4b7cf0765888c8e0dbe9177d
estefano99 <estefanobugari@gmail.com>
2021-11-08 16:23:20 -0300

    Pared

--- Assets/Scripts/Enemigos/ControlGenerador.cs
+++ Assets/Scripts/Enemigos/ControlGenerador.cs
@@ -10,7 +10,7 @@ public class ControlGenerador : MonoBehaviour
     // public int enemigoInicial = 1; // cant de enemigos en la primer oleada
     public int numeroMaxOleadas = 10; // la cant maxima de oleadas del nivel
     public int oleadaInicial = 1; //oleada en la que comienza a instanciar enemigos
-    private int oleadaActual = 1; //en que oleada me encuentro
+    public int oleadaActual = 1; //en que oleada me encuentro
     int cantidadEnemigos = 1;//este es el entero de los enemigos instanciados
     // Start is called before the first frame update
     void Start()
---
+++ Assets/Scripts/Pared.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pared : MonoBehaviour
+{
+    public int ronda;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ronda = GetComponent<ControlGenerador>().oleadaActual;
+        if (ronda == 3)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
